ebec7e02c07a55551233716fe358e2562190259b
steve <steve.nguyen2013@gmail.com>
2022-08-20 17:34:14 -0700

    completed assignments

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -6,6 +6,27 @@ class DogHousesController < ApplicationController
     render json: dog_house
   end
 
+  def index
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: :dog_house
+  end
+
+  # def reviews_index
+  #   dog_house = DogHouse.find(params[:dog_house_id])
+  #   reviews = dog_house.reviews
+  #   render json: reviews, include: :dog_house
+  # end
+
+  # def review
+  #   review = Review.find(params[:id])
+  #   render json: review, include: :dog_house
+  # end
+
   private
 
   def render_not_found_response
